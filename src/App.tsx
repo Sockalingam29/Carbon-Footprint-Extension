6e430a5a89f241a97225a965b344eac66b384f57
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2023-02-11 22:48:55 +0530

    cookie for session consumption

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import { ChromeMessage, ChromeMessageResponse } from "./types";
 
-import './App.css';
+import "./App.css";
 
-export default function App () {
-  const [responseFromContent,setResponseFromContent] = React.useState(0);
+function getCookie(cname: string) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
+let sessionSet = false
+
+export default function App() {
+  const [responseFromContent, setResponseFromContent] = React.useState(0);
+  // const [sessionStore,setSessionStore]=React.useState(Number(localStorage.getItem('dataConsumed')) || 0 )
+
+  if(getCookie('dataConsumed')==""){
+    document.cookie = "dataConsumed=0";
+  }
+  
+
+  const [sessionStore,setSessionStore]=React.useState(Number(getCookie('dataConsumed')) )
+  // const [prevSessionStore,setPrevSessionStore]=React.useState(0)
+
+  console.log("curr session "+sessionStore);
+
+  // console.log("curr session "+sessionStore);
+  // chrome.storage.local.set({ key: "value" }).then(() => {
+  //   console.log("Value is set to ");
+  // });
+
+  
   React.useEffect(() => {
     /**
      * We can't use "chrome.runtime.sendMessage" for sending messages from React.
      * For sending messages from React we need to specify which tab to send it to.
      */
-    chrome.tabs && chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, tabs => {
-      /**
-       * Sends a single message to the content script(s) in the specified tab,
-       * with an optional callback to run when a response is sent back.
-       *
-       * The runtime.onMessage event is fired in each content script running
-       * in the specified tab for the current extension.
-       */
-      chrome.tabs.sendMessage(
-        tabs[0].id || 0,
-        { type: 'GET_DOM' } as ChromeMessage,
-        (response: ChromeMessageResponse) => {
-          console.log(response)
-          setResponseFromContent(response.transferSize)
-        });
-    });
-  });
+    chrome.tabs &&
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true,
+        },
+        (tabs) => {
+          chrome.tabs.sendMessage(
+            tabs[0].id || 0,
+            { type: "GET_DOM" } as ChromeMessage,
+            (response: ChromeMessageResponse) => {
+              console.log(response);
+              let currPageSize = response.transferSize;
+              let totalSize = sessionStore
+              console.log("currPageSize "+currPageSize+" responseFromContent "+responseFromContent)
+              if(!sessionSet && currPageSize!=0){
+              totalSize += currPageSize;
+                sessionSet = true
+            }
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>Response from content:</p>
-                <p>
-                    {responseFromContent}
-                </p>
-            </header>
-        </div>
-    );
-};
+              setResponseFromContent(currPageSize);
+              // setPrevSessionStore(currPageSize)
+
+              setSessionStore(totalSize);
+              document.cookie = "dataConsumed="+totalSize;
+
+              console.log("Values "+" "+currPageSize+" "+totalSize);
+              console.log("cookie "+getCookie('dataConsumed'));
+              // chrome.storage?.session?.set({ usedData: totalSize });
+            }
+          );
+        }
+      );
+  },[]);
+
+  return (
+    <div className="">
+      <header className="App-header">
+        {responseFromContent/1000 <100 ? <h3>Yay! Green website</h3> : <h3>Meh! Red website</h3>}
+        <p>Current tab consumed {(responseFromContent/1000)} KB of data which is equivalent to {responseFromContent*0.000000011} g of emissions.</p>
+        <p>Session emissions: {sessionStore*0.000000011} g</p>
+        <p>Total data consumed in this session: {(sessionStore/1000)}KB</p>
+      </header>
+    </div>
+  );
+}
--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -1,21 +1,49 @@
 import { ChromeMessage, ChromeMessageResponse } from "../types";
 let currSize=0;
 let response:ChromeMessageResponse;
-const observer = new PerformanceObserver((list) => {
 
-  list.getEntries().forEach((entry) => {
-    let temp=JSON.stringify(entry)
+window.addEventListener("load", function() {
+  console.log("In fn")
+
+  const performance = window.performance;
+  if (!performance) {
+    console.log("Not supported by browser");
+    return;
+  }
+
+  const resources =  performance.getEntriesByType("resource");
+  let totalSent = 0
+
+  resources.forEach(resource => {
+    let temp=JSON.stringify(resource)
     let entryObj = JSON.parse(temp)
-    currSize+=entryObj.transferSize
-    console.log(entryObj.name+" "+entryObj.transferSize+" "+currSize)
-    response = {
-        transferSize: currSize
-    };
-    console.log(response)
+    console.log(resource.name +" "+entryObj.transferSize)
+    totalSent += entryObj.transferSize;
   });
+  response = {
+    transferSize: totalSent
+};
+
+  console.log(`Resource sent data: ${totalSent} bytes`);
+  console.log(response)
 });
 
-observer.observe({ type: "resource", buffered: true });
+// const observer = new PerformanceObserver((list) => {
+
+//   list.getEntries().forEach((entry) => {
+//     let temp=JSON.stringify(entry)
+//     let entryObj = JSON.parse(temp)
+//     let prev=currSize
+//     currSize+=entryObj.transferSize
+//     console.log(entryObj.name+" "+entryObj.transferSize+" "+currSize)
+//     response = {
+//         transferSize: currSize
+//     };
+//     console.log(response)
+//   });
+// });
+
+// observer.observe({ type: "resource", buffered: true });
 
 
 
@@ -25,8 +53,6 @@ const messagesFromReactAppListener = (message: ChromeMessage, sender: chrome.run
         message,
         sender,
     })
-
-
     sendResponse(response);
 }
 
